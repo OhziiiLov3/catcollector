f8fb331cb489ccac85ffdb8f13820d7fe6b30a6c
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-29 22:44:31 -0800

    template: add cats to index.html

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 # Create your views here.
 
+# Temp add Ctas class
+
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+    def __str__(self):
+        return f"{self.name}"
+
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little goober', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html', {'cats': cats})
 
 def about(request):
     return render(request,'about.html')
@@ -15,18 +35,7 @@ def contact(request):
 def blog(request):
     return render(request, 'blog.html')
 
-# class Cat:
-#     def __init__(self, name, breed, description, age):
-#         self.name = name 
-#         self.breed = breed
-#         self.description = description
-#         self.age = age
-# cats = [
-#     Cat('Lolo', 'tabby', 'foul little demon', 3),
-#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-#     Cat('Raven', 'black tripod', '3 legged cat', 4)
-# ]
 
-# def cat_index(request):
-#     return render(request,'cat_index.html', {'cats': cats})
+def cat_index(request):
+    return render(request,'cat/index.html', {'cats': cats})
 
