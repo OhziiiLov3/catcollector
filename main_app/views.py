bc4ec7b61af44b51f238bd464ffd02f8e19e21d4
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-30 19:41:59 -0800

    feat: complete urls,views, templates

--- main_app/views.py
+++ main_app/views.py
@@ -17,14 +17,14 @@ class Cat:
 
 cats = [
     Cat('Lolo', 'tabby', 'foul little goober', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 7),
     Cat('Raven', 'black tripod', '3 legged cat', 4)
 ]
 
 
 
 def index(request):
-    return render(request, 'index.html', {'cats': cats})
+    return render(request, 'index.html')
 
 def about(request):
     return render(request,'about.html')
@@ -37,5 +37,5 @@ def blog(request):
 
 
 def cat_index(request):
-    return render(request,'cat/index.html', {'cats': cats})
+    return render(request,'cats/index.html', {'cats': cats})
 
