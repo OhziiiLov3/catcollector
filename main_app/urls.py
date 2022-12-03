e7e59cdd8083818e3e1662c1c4bc125cbe1c7013
ohziiilov3 <klbaskerville0520@gmail.com>
2022-12-03 10:21:46 -0800

    feat: show url, view, and template added

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns =[
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blog, name='blog'),
-    path('cats/', views.cat_index, name='cat_index')
+    path('cats/', views.cat_index, name='cat_index'),
+    path('cats/<int:cat_id>/', views.cats_show, name='cats_show')
 ]
